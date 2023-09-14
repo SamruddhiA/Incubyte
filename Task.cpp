d78b2692e474e7dc4bae5b1bd07c9c46032b75ff
Samruddhi <alekar.samruddhi@gmail.com>
2023-09-14 23:05:49 +0530

    Declaration of galactic coordinates

---
+++ Task.cpp
@@ -0,0 +1,23 @@
+// Samruddhi Alekar
+// Chandrayaan 3 Lunar Craft: Galactic Space Craft Control
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ISRO
+{
+private:
+    int x, y, z;
+    /*x for east or west location,
+    y for north or south location,
+    and z for distance above or below the galactic plane*/
+
+    char direction;
+    /* N North,
+     S South,
+     E East,
+     W West,
+     U Up,
+     D Down
+    */
+};
